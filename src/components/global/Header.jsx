5a271d98ab870e935a223221d9ff23124e682e8c
sara <sara2003me@gmail.com>
2025-09-17 19:57:09 +0300

    update routes + redeploy

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -8,11 +8,11 @@ function Header() {
     /* used in small screens */
     return (
         <nav className="bg-gray-900 lg:h-0 h-16 w-full flex justify-end">
-            <a href="#" className="lg:hidden flex items-center p-2 rounded-lg text-white group">
+            <div className="lg:hidden flex items-center p-2 rounded-lg text-white group">
                 <span className="self-center text-3xl font-semibold whitespace-nowrap">
                     <span className="text-4xl text-blue-500 capitalize !font-Courier">{firstLetter}</span>{restOfName}
                 </span>
-            </a>
+            </div>
         </nav>
     )
 }
